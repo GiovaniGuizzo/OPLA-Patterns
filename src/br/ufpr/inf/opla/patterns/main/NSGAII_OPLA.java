dccae0ec52ca00492ebfa5e239f856beaef8cc87
giovaniguizzo <giovaniguizzo@192.168.1.105>
2014-05-13 11:04:05 -0300

    Adicionado ao gitignore os arquivos de configuração do netbeans.
    ************ Revisão funcional com. Reverter aqui.

--- src/br/ufpr/inf/opla/patterns/main/NSGAII_OPLA.java
+++ src/br/ufpr/inf/opla/patterns/main/NSGAII_OPLA.java
@@ -30,9 +30,9 @@ public class NSGAII_OPLA {
 //--  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
     public static void main(String[] args) throws FileNotFoundException, IOException, JMException, ClassNotFoundException {
 
-        int runsNumber = 1; //30;
-        populationSize_ = 2; //100; 
-        maxEvaluations_ = 300; //300 geraçõeshttp://loggr.net/
+        int runsNumber = 30; //30;
+        populationSize_ = 100; //100; 
+        maxEvaluations_ = 30000; //300 geraçõeshttp://loggr.net/
 
         crossoverProbability_ = 0.0;
         mutationProbability_ = 0.1;
